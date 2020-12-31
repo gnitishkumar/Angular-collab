ef04cf37f78aba0dd86880f527b48ed11799658e
H28-dev <Hanuk8589@example.com>
2020-12-31 22:05:44 +0530

    updated_config

--- backend/doubtsforum/doubtsforum/settings.py
+++ backend/doubtsforum/doubtsforum/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '44*p62x#3&j2w*z!6%o*i87#3y_t^3e9!5dx3l3(e6_r&z+=z$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -144,3 +144,4 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
